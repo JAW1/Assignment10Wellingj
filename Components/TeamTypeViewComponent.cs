87a83b2d1329b3b3b39446d4f263ea5c860de048
JAW1 <Jakewelling@gmail.com>
2021-03-30 10:46:34 -0600

    Add project files.

---
+++ Components/TeamTypeViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Assignment10Wellingj.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10Wellingj.Components
+{
+    public class TeamTypeViewComponent : ViewComponent
+    {
+        //creates the context for program
+        private BowlingLeagueContext context;
+        public TeamTypeViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+
+        //
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData?.Values["teamtype"];
+
+            //makes sure the teams are distinct and passes the context to where it will list the teams out
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
